8136db1408687e6c84fbf6cce8f706f5b1155403
SpoiledPotato <itulashvili@yahoo.com>
2021-10-04 22:58:30 +0400

    project1 - completed vigenere on lists

--- Project-1/main.py
+++ Project-1/main.py
@@ -5,6 +5,7 @@ import vigenere_tuple
 
 myString = "attackatdawn"
 myTuple = ("irakli", "tulashvili", myString)
+myList = ["maksim", "iavich", myString]
 myKey = "LEMON"
 
 encryptedString = vigenere_string.encrypt(myString, myKey)
@@ -13,13 +14,21 @@ decriptedString = vigenere_string.decrypt(encryptedString, myKey)
 encryptedTuple = vigenere_tuple.encrypt(myTuple, myKey)
 decryptedTuple = vigenere_tuple.decrypt(encryptedTuple, myKey)
 
+
+encryptedList = vigenere_list.encrypt(myList, myKey)
+decryptedList = vigenere_list.decrypt(encryptedList, myKey)
+
 # print(myString)
 
 # print(encryptedString)
 # print(decriptedString)
 
-print(myTuple)
-print(encryptedTuple)
-print(decryptedTuple)
+# print(myTuple)
+# print(encryptedTuple)
+# print(decryptedTuple)
+
+print(myList)
+print(encryptedList)
+print(decryptedList)
 
 # print(ord("A"))
--- Project-1/vigenere_list.py
+++ Project-1/vigenere_list.py
@@ -1,5 +1,15 @@
+import vigenere_string
+
 def encrypt(L, key):
-    print(L)
+    encryptedList = []
+    for s in L:
+        encryptedString = vigenere_string.encrypt(s, key)
+        encryptedList.append(encryptedString)
+    return encryptedList
 
 def decrypt(L, key):
-    print(L)
+    decryptedList = []
+    for s in L:
+        decryptedString = vigenere_string.decrypt(s, key)
+        decryptedList.append(decryptedString)
+    return decryptedList
