5de66a48cf413289f77700364e3cc548644dbee0
SpoiledPotato <itulashvili@yahoo.com>
2021-10-04 22:59:57 +0400

    project1 - formatted documents

--- Project-1/vigenere_list.py
+++ Project-1/vigenere_list.py
@@ -1,11 +1,13 @@
 import vigenere_string
 
+
 def encrypt(L, key):
     encryptedList = []
     for s in L:
         encryptedString = vigenere_string.encrypt(s, key)
         encryptedList.append(encryptedString)
     return encryptedList
+
 
 def decrypt(L, key):
     decryptedList = []
--- Project-1/vigenere_tuple.py
+++ Project-1/vigenere_tuple.py
@@ -1,11 +1,13 @@
 import vigenere_string
 
+
 def encrypt(T, key):
     encryptedTuple = ()
     for s in T:
         encryptedString = vigenere_string.encrypt(s, key)
         encryptedTuple += (encryptedString,)
     return encryptedTuple
+
 
 def decrypt(T, key):
     decryptedTuple = ()
