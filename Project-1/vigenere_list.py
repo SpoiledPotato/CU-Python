40cb2ccb4addfdfb5bbd62d332826048debd1984
SpoiledPotato <itulashvili@yahoo.com>
2021-10-08 00:53:25 +0400

    project1 - completed new vigenere on list

--- Project-1/vigenere_list.py
+++ Project-1/vigenere_list.py
@@ -1,10 +1,10 @@
-import vigenere_string
+import Components.vigenere
 
 
 def encrypt(L, key):
     encryptedList = []
     for s in L:
-        encryptedString = vigenere_string.encrypt(s, key)
+        encryptedString = Components.vigenere.encrypt(s, key)
         encryptedList.append(encryptedString)
     return encryptedList
 
@@ -12,6 +12,22 @@ def encrypt(L, key):
 def decrypt(L, key):
     decryptedList = []
     for s in L:
-        decryptedString = vigenere_string.decrypt(s, key)
+        decryptedString = Components.vigenere.decrypt(s, key)
         decryptedList.append(decryptedString)
     return decryptedList
+
+
+inp = input("Do you want to (E)NCRYPT or (D)ECRYPT the text?: ")
+tmpList = []
+if inp == 'E' or inp == 'e':
+    n = int(input("How many elements do you want to have in list?: "))
+    for i in range(0, n):
+        tmpList.append(input("Enter element #" + str(i) + ": "))
+    encryptedList = encrypt(tmpList, input("Enter key: "))
+    print("Encrypted list:", encryptedList)
+elif inp == 'D' or inp == 'd':
+    n = int(input("How many elements do you want to have in list?: "))
+    for i in range(0, n):
+        tmpList.append(input("Enter element #" + str(i) + ": "))
+    decryptedList = decrypt(tmpList, input("Enter key: "))
+    print("Decrypted list:", decryptedList)
