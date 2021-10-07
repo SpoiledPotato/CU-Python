63c81f06c2047fdb7228ba36c638d49a60dec938
SpoiledPotato <itulashvili@yahoo.com>
2021-10-08 00:35:18 +0400

    project1 - moved main methods to vigenere.py

---
+++ Project-1/Components/vigenere.py
@@ -0,0 +1,30 @@
+def getFullKey(key, length):
+    fullKey = key
+    for i in range(len(key), length):
+        fullKey += key[i % len(key)]
+    return fullKey
+
+
+def encrypt(S, key):
+    S = S.upper()
+    key = key.upper()
+    fullKey = getFullKey(key, len(S))
+    encryptedString = ""
+    for i in range(0, len(S)):
+        a = ord(S[i])-65
+        b = ord(fullKey[i])-65
+        encryptedString += chr((a+b) % 26 + 65)
+
+    return encryptedString
+
+def decrypt(S, key):
+    S = S.upper()
+    key = key.upper()
+    fullKey = getFullKey(key, len(S))
+    decryptedString = ""
+    for i in range(0, len(S)):
+        a = ord(S[i])-65
+        b = ord(fullKey[i])-65
+        decryptedString += chr((a-b) % 26 + 65)
+
+    return decryptedString
